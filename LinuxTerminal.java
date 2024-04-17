4411659b40241288e70f82c24a8ba13cffa0d17e
ayushdayal900 <134316930+ayushdayal900@users.noreply.github.com>
2024-04-17 17:46:07 +0530

    Add files via upload

---
+++ LinuxTerminal.java
@@ -0,0 +1,12 @@
+package FDS_PROJECT;    
+// import javax.swing.*;
+
+
+public class LinuxTerminal {
+    public static void main(String[] args) {
+        terminal t = new terminal();
+        t.command();  // init commands in an array list
+        
+    }
+}
+
